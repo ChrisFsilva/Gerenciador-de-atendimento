68383d089427c7849d4d72871b4d0a45067a1d12
ChrisFsilva <chrisspfc.silva@gmail.com>
2026-07-24 16:51:06 -0300

    Corrigindo código de Integração com o power automate

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -6,13 +6,7 @@ from app.models import FilaAtendimento
 
 scheduler = BackgroundScheduler()
 POWER_AUTOMATE_URL = (
-    "https://e6f51598a921ebffb719475e56d7f7.e2.environment.api.powerplatform.com"
-    "/powerautomate/automations/direct/cu/01/workflows/5ae96b0ceba24a8ca4978a4ca6ff1b92"
-    "/triggers/manual/paths/invoke"
-    "?api-version=1"
-    "&sp=%2Ftriggers%2Fmanual%2Frun"
-    "&sv=1.0"
-    "&sig=SEU_SIG"
+    "https://e6f51598a921ebffb719475e56d7f7.e2.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/01/workflows/5ae96b0ceba24a8ca4978a4ca6ff1b92/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yoes7hhBGFnXYStnptaT7Ixv_i7fM4iZHdAWnwh30oY"
 )
 
 
@@ -45,17 +39,22 @@ def remover_usuarios_inativos():
             "Remover usuario?", agora > limite
             )
             print(f"Removendo usuário -- {usuario.usuario}")
-            print('Mensagem enviada ao Power automate')
-            
-            requests.post(
+
+
+            resposta = requests.post(
                 POWER_AUTOMATE_URL,
                 json={
                     "usuario": usuario.usuario_id,
                     "loja": usuario.loja,
                     "ultima_atividade": str(usuario.ultima_atividade)
                 },
-                timeout=10
-                
+                timeout=30
+            )
+
+            print(
+                "Power Automate:",
+                resposta.status_code,
+                resposta.text
             )
 
             usuario.ativo = False

90b2fc1e9af65e56f0a381b91c89ebf3015c096a
ChrisFsilva <chrisspfc.silva@gmail.com>
2026-07-27 10:35:34 -0300

    teste de timer para anuncio de queda no teams

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -1,12 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 import requests
 from app.database import SessionLocal
 from app.models import FilaAtendimento
 
 scheduler = BackgroundScheduler()
+
 POWER_AUTOMATE_URL = (
-    "https://e6f51598a921ebffb719475e56d7f7.e2.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/01/workflows/5ae96b0ceba24a8ca4978a4ca6ff1b92/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=yoes7hhBGFnXYStnptaT7Ixv_i7fM4iZHdAWnwh30oY"
+    "https://e6f51598a921ebffb719475e56d7f7.e2.environment.api.powerplatform.com:443"
+    "/powerautomate/automations/direct/cu/01/workflows/5ae96b0ceba24a8ca4978a4ca6ff1b92"
+    "/triggers/manual/paths/invoke"
+    "?api-version=1"
+    "&sp=%2Ftriggers%2Fmanual%2Frun"
+    "&sv=1.0"
+    "&sig=yoes7hhBGFnXYStnptaT7Ixv_i7fM4iZHdAWnwh30oY"
 )
 
 
@@ -15,32 +22,31 @@ def remover_usuarios_inativos():
     db = SessionLocal()
 
     try:
+
         agora = datetime.now()
-        limite = agora-timedelta(minutes=1)
+
+        limite_notificacao = agora - timedelta(minutes=1)
+        limite_remocao = agora - timedelta(minutes=3)
 
         usuarios = (
             db.query(FilaAtendimento)
             .filter(
                 FilaAtendimento.ativo == True,
-                FilaAtendimento.ultima_atividade < limite
+                FilaAtendimento.ultima_atividade < limite_notificacao
             )
             .all()
         )
 
-
         for usuario in usuarios:
-            
+
             print(
-            "Usuário:", usuario.usuario_id,
-            "login", usuario.data_entrada,
-            "Última atividade:", usuario.ultima_atividade,
-            "Agora é:", agora,
-            "Limite:", limite,
-            "Remover usuario?", agora > limite
+                "Usuário:", usuario.usuario_id,
+                "Login:", usuario.data_entrada,
+                "Última atividade:", usuario.ultima_atividade,
+                "Agora:", agora
             )
-            print(f"Removendo usuário -- {usuario.usuario}")
 
-
+            # Envia aviso após 8 minutos de inatividade
             resposta = requests.post(
                 POWER_AUTOMATE_URL,
                 json={
@@ -58,7 +64,12 @@ def remover_usuarios_inativos():
                 resposta.text
             )
 
-            usuario.ativo = False
+            # Remove somente após 10 minutos
+            if usuario.ultima_atividade < limite_remocao:
+
+                print(f"Removendo usuário {usuario.usuario_id}")
+
+                usuario.ativo = False
 
         db.commit()
 
@@ -71,3 +82,5 @@ scheduler.add_job(
     "interval",
     seconds=50
 )
+
+scheduler.start()
